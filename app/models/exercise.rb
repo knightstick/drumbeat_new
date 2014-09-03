cbbf83c1f730bd39407ca773945e5a2f0ba85223
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-03 10:38:17 +0000

    Refactor images

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,9 @@
 class Exercise < ActiveRecord::Base
   
+  def image_url
+    "http://www.freedrumlessons.com/media/drum-lessons/drum-rudiments/#{self.dashify}-1.gif"
+  end
+  
   def dashify
     self.name.downcase.split(' ').join('-')
   end
