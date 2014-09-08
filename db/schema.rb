e1d77c31225cf801be3232c834b244c69b637546
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-08 05:22:14 +0000

    Add validations to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903230553) do
+ActiveRecord::Schema.define(version: 20140908051646) do
 
   create_table "exercises", force: true do |t|
     t.string   "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20140903230553) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "name"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
