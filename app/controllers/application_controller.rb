a59f460a1b41940307dbd17bf352166ac3d962f9
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-08 06:03:22 +0000

    Finish user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout :choose_layout
+  # layout :choose_layout
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -21,13 +21,13 @@ class ApplicationController < ActionController::Base
   
   private
   
-    def choose_layout
-      if current_user
-        "logged_in"
-      else
-        "application"
-      end
-    end
+    # def choose_layout
+    #   if current_user
+    #     "logged_in"
+    #   else
+    #     "application"
+    #   end
+    # end
   
 
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,17 +10,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "New user created!"
+      flash[:success] = "New user created!"
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:alert] = "There was a problem creating your account."
       render "new"
     end
   end
   
   private
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
     end
 end
