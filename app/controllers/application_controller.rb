9a5805f3e8465f563f366ccb1667cdfc90e59263
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-08 06:20:48 +0000

    Change login link to logout when signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # layout :choose_layout
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -17,19 +16,4 @@ class ApplicationController < ActionController::Base
   end
   
   helper_method :current_user, :title_helper
-  
-  
-  private
-  
-    # def choose_layout
-    #   if current_user
-    #     "logged_in"
-    #   else
-    #     "application"
-    #   end
-    # end
-  
-
-  
-   
 end
