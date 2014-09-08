5946b9b9c7ad62c799f09a2082e2e6205d158988
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-08 04:45:07 +0000

    Add site layout test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  test "layout links" do
+    get root_path
+    assert_template 'home/index'
+    assert_select "a[href=#{root_path}]"
+    assert_select "a[href=#{login_path}]"
+    assert_select "a[href=#{signup_path}]"
+  end
+end
+
