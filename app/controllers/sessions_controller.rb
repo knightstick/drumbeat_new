c4c13af592985563d7384c4c3244fa8ebaecfcec
Chris Jewell <chrisjohnjewell@gmail.com>
2014-09-17 07:33:39 +0000

    Make flash look nicer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      flash[:notice] = "Logged in"
+      flash[:notice] = "Logged in as #{@user.name}"
       session[:user_id] = @user.id
       @user.assign_daily
       redirect_to @user
     else
-      flash[:error] = "There was an error."
+      flash[:error] = "Could not log you in."
       render "new"
     end
     
