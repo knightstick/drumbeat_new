ec75730ae340a0640803afbde9533eb551856807
Chris Jewell <chrisjohnjewell@gmail.com>
2014-10-03 16:08:31 +1000

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Workspace::Application.routes.draw do
   root 'home#index'
   
+  resources :exercises, only: [:index, :show]
+
+  resources :users, except: [:index] do
+    resources :scorecards, only: [:index, :show, :update]
+  end
+
+
+
   get "logout" => "sessions#destroy", as: "logout"
   get "signup" => "users#new", as: "signup"
   get "login" => "sessions#new", as: "login"
   post "login" => "sessions#create"
-  
-  get "exercises" => 'exercises#index'
-  get "exercises/:id" => 'exercises#show', as: 'exercise'
   
   get "profile" => 'users#profile', as: 'profile'
   get "assign/:id" => 'users#assign_new_daily', as: 'assign_daily'
@@ -15,22 +20,14 @@ Workspace::Application.routes.draw do
   get "assign_monthly/:id" => 'users#assign_new_monthly', as: 'assign_monthly'
   post "assign/:id" => 'users#assign', as: 'assign'
 
-  
-  get "users/:user_id/scorecards/:id" => 'scorecards#show', as: 'scorecard'
-  post "users/:user_id/scorecards/:id" => 'scorecards#update'
   get "scorecards" => 'scorecards#index', as: 'scorecards'
   
-
-
   get "pick_new_daily" => "users#assign_new_daily", as: 'assign_new_daily'
   get "pick_new_weekly" => "users#assign_new_weekly", as: 'assign_new_weekly'
   get "pick_new_monthly" => "users#assign_new_monthly", as: 'assign_new_monthly'
   
   get "users/:user_id/scorecards/:id/reset" => "scorecards#reset", as: 'reset_scorecard'
   
-  resources :users, except: [:index] do
-    resources :scorecards, only: [:index, :show, :update]
-  end
   
   # get "/login" => "sessions#new"
   # post "/login" => "sessions#create"
